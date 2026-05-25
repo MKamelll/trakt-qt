9692f95afdd203970322fba82e277f8c8b913b52
Mina Kamel <minallkamel@gmail.com>
2026-05-25 04:17:17 +0300

    wip

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,3 +1,15 @@
 #include "loginwindow.hpp"
+#include <QVBoxLayout>
+#include <QPushButton>
+#include "traktclient.hpp"
 
-LoginWindow::LoginWindow(QWidget *parent) : QWidget(parent) {}
+LoginWindow::LoginWindow(QWidget *parent) : QWidget(parent) {
+    auto vbox = new QVBoxLayout(this);
+
+    auto loginBtn = new QPushButton("Login", this);
+
+    vbox->addWidget(loginBtn, 25, Qt::AlignCenter);
+
+    connect(loginBtn, &QPushButton::clicked, this,
+            [=]() { TraktClient::instance()->authenticate(); });
+}
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,28 +1,28 @@
 #include "mainwindow.hpp"
 #include <QPushButton>
 #include <QVBoxLayout>
+#include "loginwindow.hpp"
+#include <QStackedWidget>
 #include "traktclient.hpp"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("trakt");
     resize(640, 420);
 
-    TraktClient::instance()->search("breaking bad");
+    auto loginwindow = new LoginWindow;
+    auto homeWindow = new QWidget;
 
-    connect(TraktClient::instance(), &TraktClient::searchDone, this,
-            [=](QList<StandardShow> arr) { qDebug() << arr << "\n"; });
+    auto stackedWidget = new QStackedWidget;
+    stackedWidget->addWidget(loginwindow);
+    stackedWidget->addWidget(homeWindow);
 
-    auto centralWidget = new QWidget;
-    auto vbox = new QVBoxLayout(centralWidget);
+    if (TraktClient::instance()->isAuthenticated()) {
+        stackedWidget->setCurrentWidget(homeWindow);
+    } else {
+        stackedWidget->setCurrentWidget(loginwindow);
+    }
 
-    auto loginBtn = new QPushButton("Login", this);
-
-    vbox->addWidget(loginBtn, 25, Qt::AlignCenter);
-
-    connect(loginBtn, &QPushButton::clicked, this,
-            [=]() { TraktClient::instance()->authenticate(); });
-
-    setCentralWidget(centralWidget);
+    setCentralWidget(stackedWidget);
 }
 
 MainWindow::~MainWindow() {}
